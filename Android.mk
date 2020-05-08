14cd6bd5efa6244de3e05f13c2ba11576155adf6
celtare21 <celtare21@gmail.com>
2020-05-08 13:45:42 +0200

    Update script
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,1 +1,20 @@
 include $(call all-subdir-makefiles)
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := anxfilecheck.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := init/anxfilecheck.sh
+LOCAL_VENDOR_MODULE    := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := anxcamera.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init/anxcamera.rc
+LOCAL_VENDOR_MODULE    := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
